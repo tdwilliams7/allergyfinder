20faf9c4e03478ef40dd800be56a9efa574af745
Troy Williams <troyDonwilliams@gmail.com>
2018-04-06 14:17:50 -0500

    users adding to db and email is unique now.

--- server/user/userRoutes.js
+++ server/user/userRoutes.js
@@ -1,10 +1,26 @@
 const express = require('express');
-
 const userRouter = express.Router();
+const User = require('./userSchema');
 
-userRouter.post('/', (req, res) => {
+userRouter.post('/signUp', (req, res) => {
   const { name, email, password } = req.body;
-  res.send({ name, email, password });
+  if (!name) {
+    res.status(422).json({ err: 'Please include a Name' });
+  } else if (!email) {
+    res.status(422).json({ err: 'please include an email' });
+  } else if (!password) {
+    res.status(422).json({ err: 'please include a password' });
+  } else {
+    const user = new User({ name, email, password });
+    user
+      .save()
+      .then(signedUp => {
+        res.status(200).json({ newUser: signedUp });
+      })
+      .catch(err => {
+        res.status(500).json({ err });
+      });
+  }
 });
 
 module.exports = userRouter;
--- server/user/userSchema.js
+++ server/user/userSchema.js
@@ -6,11 +6,19 @@ const BCRYPT_COST = 11;
 const UserSchema = new Schema({
   name: { type: String, required: true },
   password: { type: String, required: true, index: true },
-  email: { type: String, required: true, index: true }
+  email: { type: String, required: true, index: true, unique: true }
 });
 
-UserSchema.pre('save', function() {
-  console.log(this.password);
+UserSchema.pre('save', function(next) {
+  bcrypt
+    .hash(this.password, BCRYPT_COST)
+    .then(hashed => {
+      this.password = hashed;
+      next();
+    })
+    .catch(err => {
+      console.log('error hashing password');
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
